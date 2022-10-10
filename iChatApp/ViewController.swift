1407f9199e5d03d15295fa1a93783e2f3ce0f0e8
Ivan Lyaskovets <lyaskovets.iv@gmail.com>
2022-10-10 18:21:39 +0300

    Refactor: initial init without main.storyboard and with canvas extension

--- iChatApp/ViewController.swift
+++ iChatApp/ViewController.swift
@@ -12,8 +12,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-
-
+    
+    
 }
+
+
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider{
+    
+    static var previews: some View{
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable{
+       
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: Context) -> ViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+}
+
 
