c758a98af9cb1a01ce8f89dc0fc168fa28428b6e
Ivan Lyaskovets <lyaskovets.iv@gmail.com>
2022-10-17 17:47:09 +0300

    Refactor: some refactor actions in whole project

---
+++ iChatApp/Controllers/ConversationsVC.swift
@@ -0,0 +1,18 @@
+//
+//  ConversationsViewController.swift
+//  iChatApp
+//
+//  Created by Ivan Lyaskovets on 14.10.2022.
+//
+
+import UIKit
+
+class ConversationsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
---
+++ iChatApp/Controllers/PeopleVC.swift
@@ -0,0 +1,19 @@
+//
+//  PeopleViewController.swift
+//  iChatApp
+//
+//  Created by Ivan Lyaskovets on 14.10.2022.
+//
+
+import UIKit
+
+class PeopleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+    }
+    
+
+}
